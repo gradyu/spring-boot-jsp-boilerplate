1b65ffeec2ace0ed672e879aab6983710ad1a892
Grady <yhwei876@gmail.com>
2018-10-20 14:40:04 +0800

    refactor project.

---
+++ src/main/java/com/grady/jsp/JspApplication.java
@@ -0,0 +1,13 @@
+package com.grady.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class JspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspApplication.class, args);
+	}
+}
--- src/test/java/com/grady/jsp/JspApplicationTests.java
+++ src/test/java/com/grady/jsp/JspApplicationTests.java
@@ -1,4 +1,4 @@
-package com.grady.jspboilerplate;
+package com.grady.jsp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class JspboilerplateApplicationTests {
+public class JspApplicationTests {
 
 	@Test
 	public void contextLoads() {
